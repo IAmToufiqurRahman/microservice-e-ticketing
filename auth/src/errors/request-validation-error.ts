60daa095613b090f7fc168d8b0b91b9616df41e5
IAmToufiqurRahman <toufiqurrahmanaust@gmail.com>
2022-01-20 23:51:38 +0600

    scaffolded routes for auth service and designed a custom error handler

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,25 @@
+import { ValidationError } from 'express-validator'
+
+import { CustomError } from './custom-error'
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400
+
+  constructor(public errors: ValidationError[]) {
+    super('Invalid request parameters')
+
+    // Only because we are extending a built-in class in typescript
+    Object.setPrototypeOf(this, RequestValidationError.prototype)
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return {
+        message: err.msg,
+        field: err.param
+
+        // this msg and param property is coming ferom ValidationError[] of RequestValidationError
+      }
+    })
+  }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import { json } from 'body-parser'
+import 'express-async-errors'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
@@ -15,8 +18,22 @@ app.use(signinRouter)
 app.use(signupRouter)
 app.use(signoutRouter)
 
+app.get('*', async (req, res) => {
+  throw new NotFoundError()
+})
+
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('listening to the port 3000!')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+  } catch (error) {
+    console.log(error)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening to the port 3000!')
+  })
+}
+
+start()
